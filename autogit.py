26fc85ac83f0774978784c35b1cbee9fe8c9d5d7
ktxdev <sean.ktxdev@gmail.com>
2022-08-13 01:21:45 +0200

    Added functionality to create github repo and push local repo to the remote repo

--- autogit.py
+++ autogit.py
@@ -1,12 +1,20 @@
+from lib2to3.pgen2 import driver
 import os
 import subprocess
 
 from sys import argv
 from pathlib import Path
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 from config import setup, get_configs, write_configs, CONFIG_HELP_TEXT, AVAILABLE_CONFIG_OPTIONS, PROJECTS_DIR_KEY
 
 GITHUB_LOGIN_URL = "https://github.com/login"
+NEW_REPO_GITHUB_URL = "https://github.com/new"
 AVAILABLE_ACTIONS = ['config', 'create', '--help']
 ACTIONS_HELP_TEXT = "Usage: autogit [--help] <command> [<args>]\
     \n\nThese are common Autogit commands used in various situations:\
@@ -70,7 +78,8 @@ def main():
             # Move to the configured projects dir
             os.chdir(configs[PROJECTS_DIR_KEY])
             # Create the project dir
-            project_dir = Path.joinpath(projects_dir, argv[len(argv) - 1])
+            project_name = argv[len(argv) - 1]
+            project_dir = Path.joinpath(projects_dir, project_name)
 
             if project_dir.exists():
                 if argv.__contains__("-f"):
@@ -82,15 +91,42 @@ def main():
             project_dir.mkdir()
             # Move to the project dir
             os.chdir(project_dir)
-            # Create a ReadMe.md file
-            readme_file = Path.joinpath(project_dir, "README.md")
-            readme_file.touch()
             # Initialize a git repository
             subprocess.call(["git", "init"])
             # Create a repo on github
+            username = input('GitHub username:')
+            password = input('GitHub password:')
+            driver = webdriver.Chrome(ChromeDriverManager().install())
+            driver.get(GITHUB_LOGIN_URL)
+            element = driver.find_element(By.ID, "login_field")
+            element.clear()
+            element.send_keys(username)
+            element = driver.find_element(By.ID, 'password')
+            element.clear()
+            element.send_keys(password)
+            element.send_keys(Keys.ENTER)
+
+            driver.get(NEW_REPO_GITHUB_URL)
+            element = driver.find_element(By.ID, 'repository_name')
+            element.clear()
+            element.send_keys(project_name)
+
+            # element = driver.find_element(By.CLASS_NAME, 'btn')
+            element.submit()
+
+            # Create a ReadMe.md file
+            readme_file = Path.joinpath(project_dir, "README.md")
+            readme_file.touch()
+            readme_file.write_text('# ' + project_name)
+
             # Add the remote to local project
+            subprocess.call(["git", "remote", "add", "origin", f"git@github.com:{username}/{project_name}.git"])
+            subprocess.call(["git", "branch", "-M", "main"])
             # Make initial commit
+            subprocess.call(["git", "add", "."])
+            subprocess.call(["git", "commit", "-m", "Initial commit"])
             # Push to github
+            subprocess.call(["git", "push", "-u", "origin", "main"])
 
 
 if __name__ == '__main__':
